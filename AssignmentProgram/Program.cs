1fe6f36393b734e7ac666ab9f7ec0a8a6348236d
Harshal Bokade <harshalbokade984@gmail.com>
2021-07-29 17:52:02 +0530

    Add project files.

---
+++ AssignmentProgram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AssignmentProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            num1 = 10;
+            num2 = ++num1;
+            Console.WriteLine("Assigning num2 by pre-increment");
+            Console.WriteLine("num1: {0} \n num2: {1}", num1,num2);
+
+            num2 = num1++;
+            Console.WriteLine("Assigning num2 by pre-increment");
+            Console.WriteLine("num1: {0} \n num2: {1}", num1, num2);
+
+        }
+    }
+}
